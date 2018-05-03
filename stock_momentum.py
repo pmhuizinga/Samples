5941bc6fcecb4b94f01444476f9ea06bc1979494
pmhuizinga <32456342+pmhuizinga@users.noreply.github.com>
2018-05-03 07:56:10 +0200

    Add files via upload

--- stock_momentum.py
+++ stock_momentum.py
@@ -21,8 +21,8 @@ import numpy as np
 
 # Variables
 nr_of_stocks = 2 # Number of best performing stocks
-nr_of_cumulative_periods = 3 # Number of periods to calculate cumulative returns
-rebalance_frequency = 3 # Number of periods to rebalancing
+nr_of_cumulative_periods = 2 # Number of periods to calculate cumulative returns
+rebalance_frequency = 2 # Number of periods to rebalancing
 transaction_costs = 0.01 # 1%
 
 file = 'AEX-data.xlsx'
@@ -33,7 +33,7 @@ df = pd.read_excel(file, sheet, header=0)
 df = df[1:]
 
 # even tijdelijk het bereik verkleinen tbv test
-# df = df.iloc[0:14,0:5]
+# df = df.iloc[0:10,0:4]
 
 # Sortering op datum
 df.sort_values(by=['Name'])
@@ -41,8 +41,6 @@ df.sort_values(by=['Name'])
 # Maak arrays aan voor cumulatieve rendementen en voor gewichten
 df_cum = df.copy(deep=True) # array voor cumulatieve 
 df_contribution = df.copy(deep=True)
-#df_weight = df.copy(deep=True) # array voor gewichten
-#df_weight_rebal = df.copy(deep=True) # array voor gewichten
 rows, cols = df.shape # grootte van het bereik
 
 # replace market values in df with monthly returns and append cumulative returns to df_cum dataframe.
@@ -66,7 +64,10 @@ for x in range(weights.shape[0]): # rows
 for x in range(df.shape[0]):
     for y in range(df.shape[1])[1:]:
         df_contribution.iloc[x,y] = df.iloc[x,y] * weights_rebal[x][y-1]
-        
-# nu nog de contributies sommeren op regelniveau en vervolgens cumululeren op kolom niveau
+
+# let op: aanpassen naar sumproduct (1+x)
+result = df_contribution.iloc[:,1:].sum(axis=1).cumsum().iloc[-1]
+
+
 
 
